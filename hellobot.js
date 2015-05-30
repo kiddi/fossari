8c604e88f4826d98f5eb162805c0a3ee1551f783
Kiddi <kiddi@plainvanillagames.com>
2015-05-30 00:51:06 +0000

    added brackets to payload

--- hellobot.js
+++ hellobot.js
@@ -4,11 +4,11 @@ module.exports = function (req, res, next) {
 	var userName = req.body.user_name;
 	
 	if (n === 5) {
-		var botPayload = "text": "FÖSSARI! https://www.youtube.com/watch?v=bsrc3vCL13E";
+		var botPayload = { "text": "FÖSSARI! https://www.youtube.com/watch?v=bsrc3vCL13E" };
 	} else if (n === 6) {
-		var botPayload = "text": "LÖLLARI! https://www.youtube.com/watch?v=mp-IZEFqrG0";
+		var botPayload = { "text": "LÖLLARI! https://www.youtube.com/watch?v=mp-IZEFqrG0" };
 	} else {
-		var botPayload = "text": "Bara leiðinlegur dagur í dag :-(";
+		var botPayload = { "text": "Bara leiðinlegur dagur í dag :-(" };
 	}
 
 	//var botPayload = (n === 6) ?
