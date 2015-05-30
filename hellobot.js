e03c97aa9ce33700a26c8b72b5e16414c2fc99eb
Kiddi <kiddi@plainvanillagames.com>
2015-05-30 00:46:57 +0000

    added "text": to payload

--- hellobot.js
+++ hellobot.js
@@ -4,13 +4,13 @@ module.exports = function (req, res, next) {
 	var userName = req.body.user_name;
 	
 	if (n === 5) {
-		var botPayload = 'FÖSSARI! https://www.youtube.com/watch?v=bsrc3vCL13E';
+		var botPayload = "text": "FÖSSARI! https://www.youtube.com/watch?v=bsrc3vCL13E";
 	} else if (n === 6) {
-		var botPayload = 'LÖLLARI! https://www.youtube.com/watch?v=mp-IZEFqrG0';
+		var botPayload = "text": "LÖLLARI! https://www.youtube.com/watch?v=mp-IZEFqrG0";
 	} else {
-		var botPayload = 'Bara leiðinlegur dagur í dag :-(';
+		var botPayload = "text": "Bara leiðinlegur dagur í dag :-(";
 	}
-	
+
 	//var botPayload = (n === 6) ?
 	//'FÖSSARI! https://www.youtube.com/watch?v=bsrc3vCL13E' : 'Enginn fössari :-('
 
