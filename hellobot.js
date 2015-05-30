40c7295f9e0786827e26429029a1f4e699606539
Kiddi <kiddi@plainvanillagames.com>
2015-05-30 00:42:05 +0000

    added fössari and löllari and boring days

--- hellobot.js
+++ hellobot.js
@@ -1,8 +1,24 @@
 module.exports = function (req, res, next) {
+	var d = new Date();
+	var n = d.getDay();
 	var userName = req.body.user_name;
-	var botPayload = {
-		text : 'FÖSSARI! https://www.youtube.com/watch?v=bsrc3vCL13E'
-	};
+	
+	if (n === 5) {
+		var botPayload = 'FÖSSARI! https://www.youtube.com/watch?v=bsrc3vCL13E';
+	} else if (n === 6) {
+		var botPayload = 'LÖLLARI! https://www.youtube.com/watch?v=mp-IZEFqrG0';
+	} else {
+		var botPayload = 'Bara leiðinlegur dagur í dag :-(';
+	}
+	
+	//var botPayload = (n === 6) ?
+	//'FÖSSARI! https://www.youtube.com/watch?v=bsrc3vCL13E' : 'Enginn fössari :-('
+
+
+
+	//var botPayload = {
+	//	text : 'FÖSSARI! https://www.youtube.com/watch?v=bsrc3vCL13E'
+	//};
 
 	// avoid infinite loop
 	if (userName !== 'slackbot') {
